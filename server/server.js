a37cca8ef5fb1e516855ddb40f1edfdce8b35612
Irman Fahrezy <irman.fahrezy07998@gmail.com>
2026-08-27 11:20:33 +0700

    update server js

--- server/server.js
+++ server/server.js
@@ -1,23 +1,7 @@
-import dns from "node:dns";
 import "./config/env.js";
-
 import app from "./app.js";
-
 import connectDB from "./config/database.js";
 
-const startServer = async () => {
-  try {
-    dns.setServers(["1.1.1.1"]);
-    await connectDB();
+await connectDB();
 
-    app.listen(process.env.PORT, () => {
-      console.log("Server started successfully");
-    });
-  } catch (error) {
-    console.error("Failed to start server:", error);
-
-    process.exit(1);
-  }
-};
-
-startServer();
+export default app;
