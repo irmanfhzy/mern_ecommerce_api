88aab087607ee4dbb999129c259533f8a5d56ea9
Irman Fahrezy <irman.fahrezy07998@gmail.com>
2026-08-27 14:59:35 +0700

    try to fix database

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
+console.log("🔥 SERVER.JS LOADED");
+
 import "./config/env.js";
 import app from "./app.js";
 import connectDB from "./config/database.js";
 
+console.log("🔥 BEFORE CONNECT DB");
+
 await connectDB();
+
+console.log("🔥 AFTER CONNECT DB");
 
 export default app;
